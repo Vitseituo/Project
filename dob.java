835f01d4556979455a9925966324b6a03f11075b
Vitseituo <156065904+Vitseituo@users.noreply.github.com>
2024-01-30 15:08:23 +0530

    hey

--- dob.java
+++ dob.java
@@ -1,6 +1,6 @@
 public class main{
-    public static void main(string[] args){
-        string dob = "01/01/2005";
+    public static void main(String[] args){
+        String dob = "01/01/2005";
         System.out.println("My date of birth is " + dob);
     }
 }
